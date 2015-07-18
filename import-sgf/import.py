1e111bc1dd81c3e9588772a5186b84d9864b6710
Chris LaRose <chris.larose@mycase.com>
2015-07-18 16:23:42 -0700

    cjl - Add import script

--- import-sgf/import.py
+++ import-sgf/import.py
@@ -1,4 +1,65 @@
+import fnmatch
+import os
+import uuid
+
 import psycopg2
+import psycopg2.extras
+
+def sgf_files():
+    parent = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'sgf')
+    for root, dirnames, filenames in os.walk(parent):
+        for filename in fnmatch.filter(filenames, '*.sgf'):
+            yield os.path.join(root, filename)
+
+def raw_sgf_events(filenames):
+    for sgf_filename in filenames:
+        print('opening file {}'.format(sgf_filename))
+        f = open(sgf_filename, 'r')
+        data = {
+            'filename': os.path.basename(sgf_filename),
+            'contents': f.read()
+        }
+        yield data
+
+def save_event(cursor, id, stream_id, data):
+    print('saving with id {} to stream {}'.format(id, stream_id))
+    print('data: {}'.format(data))
+    row = (str(id), stream_id, psycopg2.extras.Json(data))
+    try:
+        cursor.execute("INSERT INTO events (id, stream_id, data) VALUES (%s, %s, %s)", row)
+    except psycopg2.IntegrityError:
+        print("DETECTED DUPLICATE")
+
+def get_stream_id(cursor, stream_name):
+    cursor.execute("SELECT id FROM streams WHERE name = %s", (stream_name,))
+    result = cursor.fetchone()
+    if result is None:
+        cursor.execute("INSERT INTO streams (name) VALUES (%s) RETURNING id",
+                       (stream_name,))
+        result = cursor.fetchone()
+    return result[0]
+
+def save_events(cursor, events):
+    ns = uuid.UUID('154a6113-5503-45c2-90f6-030393f91d43')
+    stream_id = get_stream_id(cursor, 'raw_sgf')
+    print("STREAM_ID: {}".format(stream_id))
+
+    for event in events:
+        print('saving event {}'.format(event['filename']))
+        id = uuid.uuid5(ns, event['contents'])
+        save_event(cursor, id, stream_id, event)
 
 if __name__ == "__main__":
-    print("hello")
+    con = psycopg2.connect(dbname='postgres',
+                           user='postgres',
+                           host='postgres',
+                           password='')
+    con.autocommit = True
+    cursor = con.cursor()
+
+    filenames = sgf_files()
+    events = raw_sgf_events(filenames)
+    save_events(cursor, events)
+
+    cursor.close()
+    con.close()
